c3fc6bee77e762696e252544f49f7c488a6e7092
Álisson Santana <alissonsantana16608@gmail.com>
2022-10-10 11:45:38 -0300

    node.js inicio

---
+++ inicio/app.js
@@ -0,0 +1,19 @@
+var calculadora = require("./calculadora");
+
+console.log(calculadora.mult(10, 20));
+console.log(calculadora.soma(20, 40));
+calculadora.nome = "Calculadora do Alisson!";
+console.log(calculadora.nome);
+
+/*var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello word!");
+console.log("Meu nome é Alisson!");
+console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+if (mostrarSite) {
+    // Se mostra site
+    console.log(site); // Print o site
+}
+*/
